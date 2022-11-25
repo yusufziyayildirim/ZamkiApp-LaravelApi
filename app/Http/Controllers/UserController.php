a4a24b334f6003e24b958a94a9c9005eae30a6b7
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-11-25 15:18:00 +0300

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 use App\Models\User;
+use App\Models\NativeIn;
+use App\Models\AlsoSpeaking;
+use App\Models\Learning;
 use App\Http\Controllers\Response;
 use Illuminate\Http\Request;
 
@@ -16,6 +19,7 @@ class UserController extends Controller
         ->get();
         return Response::withData(true, 'Get all user', $user, 200);
     }
+
     public function getUser(Request $request){
         $user = User::where('email', $request->email)
         ->with('NativeIn','AlsoSpeaking','Learning')
@@ -23,5 +27,47 @@ class UserController extends Controller
         ->orderBy('created_at', 'desc')
         ->first();
         return Response::withData(true, 'Get user', $user, 200);
+    }
+
+    public function setUserLanguage(Request $request){
+        $user = auth()->user();
+        NativeIn::create([
+            'user_id'=>$user->id,
+            'lang'=>$request->nativeIn
+        ]);
+        AlsoSpeaking::create([
+            'user_id'=>$user->id,
+            'lang'=>$request->alsoSpeaking
+        ]);
+        Learning::create([
+            'user_id'=>$user->id,
+            'lang'=>$request->learning,
+            'level'=>1
+        ]);
+        return Response::withoutData(true, 'Success', 200);
+    }
+
+    public function updateNativeInLanguage(Request $request){
+        $user = auth()->user();
+        $native = NativeIn::where('user_id', $user->id)->first();
+        $native->lang = $request->nativeIn;
+        $native->save();
+        return Response::withoutData(true, 'Success', 200);
+    }
+
+    public function updateAlsoSpeakingLanguage(Request $request){
+        $user = auth()->user();
+        $native = AlsoSpeaking::where('user_id', $user->id)->first();
+        $native->lang = $request->alsoSpeaking;
+        $native->save();
+        return Response::withoutData(true, 'Success', 200);
+    }
+
+    public function updateLearningLanguage(Request $request){
+        $user = auth()->user();
+        $native = Learning::where('user_id', $user->id)->first();
+        $native->lang = $request->learning;
+        $native->save();
+        return Response::withoutData(true, 'Success', 200);
     }
 }
